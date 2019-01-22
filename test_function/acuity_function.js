baba70e40f67bbcd5821ab09cc18c99fce39e689
YAPI <yapi@Kamil-imac.localdomain>
2019-01-21 16:26:49 -0800

    adding dotEnv

---
+++ test_function/acuity_function.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+console.log(process.env.USER_ID);
+
+// get acuity data from 'https://acuityscheduling.com/api/v1/appointments?email=emailName&minDate=1991-01-01'
+
+// verify that there are appointments 
+
+    // if there are errors handle errors
+
+// if no errors then organize data
+
