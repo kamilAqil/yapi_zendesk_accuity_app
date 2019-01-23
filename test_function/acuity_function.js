5d75d8a1059ad604cb3499a14a2ef8ba06e9aad6
YAPI <yapi@Kamil-imac.localdomain>
2019-01-22 17:00:54 -0800

    end of workday push

--- test_function/acuity_function.js
+++ test_function/acuity_function.js
@@ -1,12 +1,33 @@
 require('dotenv').config();
 
-console.log(process.env.USER_ID);
+// axios
+const axios = require('axios');
+
+
 
 // get acuity data from 'https://acuityscheduling.com/api/v1/appointments?email=emailName&minDate=1991-01-01'
 
-// verify that there are appointments 
+function getAccuityData(email){
 
-    // if there are errors handle errors
+    let accuityEndpoint = `https://acuityscheduling.com/api/v1/appointments?email=${email}&minDate=1991-01-01`;
+    
+        // Make a request for a user with a given ID
+            axios.get(accuityEndpoint)
+            .then(function (response) {
+            console.log(JSON.stringify(response));
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
 
-// if no errors then organize data
+    // verify that there are appointments 
 
+        // if there are errors handle errors
+
+    // if no errors then organize data
+
+    console.log(`Data`);
+}
+
+
+getAccuityData('info@smileforeverdental.com');
