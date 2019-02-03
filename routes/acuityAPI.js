4edbd7f03954cbdd11823ec74f6be2a45489bea5
Kamil <kaqil91@gmail.com>
2019-02-03 03:13:17 -0800

    better data with ticket requester on acuity route

--- routes/acuityAPI.js
+++ routes/acuityAPI.js
@@ -4,9 +4,11 @@ const JSON = require('circular-json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-   
-    res.send(`wooo acuity route hit`);
+  console.log(`Request : ${JSON.stringify(req.query)}`)
+  let ticketRequester = req.query
+  console.log(`The ticket requester is ${JSON.stringify(ticketRequester)}`);
+  // run acuity stuff here
+  res.send(`wooo acuity route hit,`);
     // res.redirect(`${oAuthURL}`);
 });
 
