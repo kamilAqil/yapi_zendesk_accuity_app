f3b504b65335de2fb9e82e2a27eea899a7556a4e
Kamil <kaqil91@gmail.com>
2019-02-03 02:56:14 -0800

    getting zendesk client on front end then going to hit server api routes

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var zatRoute = require('./routes/zatRoute')
+var acuityAPI = require('./routes/acuityAPI')
 var app = express();
 
 // view engine setup
@@ -21,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/zat', zatRoute);
+app.use('/acuityAPI',acuityAPI);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/acuityAPI.js
+++ routes/acuityAPI.js
@@ -4,11 +4,9 @@ const JSON = require('circular-json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(`Hit home route`);
-    // get guid from req
-    console.log(`req:${JSON.stringify(req.query)}`)  
-    let guid = req.query.app_guid
-    res.render('index', { title: 'Express', GUID:`${guid}` });
+  
+   
+    res.send(`wooo acuity route hit`);
     // res.redirect(`${oAuthURL}`);
 });
 
