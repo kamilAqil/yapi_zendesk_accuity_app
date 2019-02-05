33aebcb69bf09d9f5bba1e2b5cfdcbc1a67d5672
YAPI <yapi@YAPIs-iMac-4.localdomain>
2019-02-05 10:29:48 -0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const JSON = require('circular-json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // this is where the acuity package will go 
-    console.log(`Hit home route`);
-  res.render('index', { title: 'Express' });
+  console.log(`Hit home route`);
+    // get guid from req
+    console.log(`req:${JSON.stringify(req.query)}`)  
+    let guid = req.query.app_guid
+    res.render('index', { title: 'Express', GUID:`${guid}` });
+    // res.redirect(`${oAuthURL}`);
 });
 
 module.exports = router;
