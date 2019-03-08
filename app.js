c945dab96282625857469fad6c88c3b983446072
YAPI <yapi@Kamil-imac.localdomain>
2019-03-07 16:59:49 -0800

    end of day push

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/zat', zatRoute);
 app.use('/acuityAPI',acuityAPI);
+app.use('/acuityAPI2',acuityAPI2);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
