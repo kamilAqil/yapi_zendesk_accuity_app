ff07275f723847db040d6d0d0985d963bc0781da
YAPI <yapi@Kamil-imac.localdomain>
2019-03-14 17:10:19 -0700

    going to use regular request method to call zendesk instead of zendesk tool

--- routes/routeFunctions/zendeskFunctions.js
+++ routes/routeFunctions/zendeskFunctions.js
@@ -7,17 +7,29 @@ var zendesk = new Zendesk({
     token: process.env.YOUR_ZENDESK_API_TOKEN // hfkUny3vgHCcV3UfuqMFZWDrLKms4z3W2f6ftjPT
   });
 
+//   going to use axios to make a regular api call to get organization data
+
   module.exports = {
       
           testFunction : function(){
               console.log(`beep boop the zendesk testFunction fired`)
           },
-          getOrganizationData : function(requesterOrganizationID){
+          getOrganizationData : async function getOrganizationData (requesterOrganizationID){
               console.log(`going to get organization data for ${requesterOrganizationID}`)
             //   use zendesk tool to get list of users 
-            // console.log(`zendesk tool ${JSON.stringify(zendesk.organizations)}`)
             
+            // await zendesk.organizations(requesterOrganizationID).then((data)=>{
+            //     console.log(`zendesk organization data ${data}`)
+            // })
 
             
+           
+            console.log(await zendesk.organizations.list({"id":requesterOrganizationID}).then((data)=>{
+                // console.log(`zendesk organization data ${data}`)
+            }))
         }
   }
+
+// let getOrganizationData = function(requesterOrganizationID){
+//     return New 
+// }
