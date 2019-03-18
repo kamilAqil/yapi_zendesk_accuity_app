dc380bea5ca63d6541ac2c4ea06380c5a288d298
YAPI <yapi@Kamil-imac.localdomain>
2019-03-18 16:02:12 -0700

    zendesk 401 is possibly  because of cors issues

--- routes/routeFunctions/zendeskFunctions.js
+++ routes/routeFunctions/zendeskFunctions.js
@@ -1,35 +1,46 @@
 
-var Zendesk = require('zendesk-node-api');
+let Zendesk = require('zendesk-node-api');
+let axios = require('axios');
+var Base64 = require('js-base64').Base64;
 
-var zendesk = new Zendesk({
+let zendesk = new Zendesk({
     url: process.env.YOUR_ZENDESK_URL, // https://example.zendesk.com
     email: process.env.YOUR_ZENDESK_EMAIL, // me@example.com
     token: process.env.YOUR_ZENDESK_API_TOKEN // hfkUny3vgHCcV3UfuqMFZWDrLKms4z3W2f6ftjPT
-  });
+});
 
+let zendeskURL = `https://yapi1504512150.zendesk.com/api/v2/organizations.json`
+let zendeskUsername = process.env.ZENDESK_USERNAME
+let zendeskToken = process.env.ZENDESK_TOKEN
 //   going to use axios to make a regular api call to get organization data
 
-  module.exports = {
-      
-          testFunction : function(){
-              console.log(`beep boop the zendesk testFunction fired`)
-          },
-          getOrganizationData : async function getOrganizationData (requesterOrganizationID){
-              console.log(`going to get organization data for ${requesterOrganizationID}`)
-            //   use zendesk tool to get list of users 
-            
-            // await zendesk.organizations(requesterOrganizationID).then((data)=>{
-            //     console.log(`zendesk organization data ${data}`)
-            // })
+module.exports = {
 
-            
-           
-            console.log(await zendesk.organizations.list({"id":requesterOrganizationID}).then((data)=>{
-                // console.log(`zendesk organization data ${data}`)
-            }))
-        }
-  }
+    testFunction: function () {
+        console.log(`beep boop the zendesk testFunction fired`)
+    },
+    getOrganizationData: async function getOrganizationData(requesterOrganizationID) {
+        console.log(`going to get organization data for ${requesterOrganizationID}`)
+        //   use zendesk tool to get list of users 
 
-// let getOrganizationData = function(requesterOrganizationID){
-//     return New 
-// }
+        console.log(`process.env ${process.env}`)
+
+        axios({
+            Authorization: 'Basic ' + Base64.encode(`${zendeskUsername}/token:${zendeskToken}`),
+            method: 'get',
+            url: zendeskURL,
+
+        }).then((data) => {
+            console.log(`data from zendesk api ${data}`)
+        }).catch((err) => {
+            console.log(err)
+        });
+
+
+
+
+
+
+    }
+}
+
