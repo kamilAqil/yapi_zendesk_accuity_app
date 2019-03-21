565e93db75a05a3cca63a3ff44bc6b2fbc215e78
YAPI <yapi@Kamil-imac.localdomain>
2019-03-20 17:02:49 -0700

    getting the zendesk data from a promise

--- routes/routeFunctions/zendeskFunctions.js
+++ routes/routeFunctions/zendeskFunctions.js
@@ -14,30 +14,40 @@ let zendeskUsername = process.env.ZENDESK_USERNAME
 let zendeskToken = process.env.ZENDESK_TOKEN
 //   going to use axios to make a regular api call to get organization data
 
+
+let getOrganizationPromise =  function(organizationID){
+    return new Promise(()=>{
+
+        var request = require('request');
+
+        let options = {
+            url: `https://yapi1504512150.zendesk.com/api/v2/organizations.json`,
+            auth: {
+                'user': 'kamil.aqil@yapicentral.com/token',
+                'pass': 'SEDAk49Im98efpxtt950zWlyvYMgPc3zff7NtG3D'
+            }
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(`body came back`);
+            }
+        }
+
+        request(options, callback);
+    });
+}
+
 module.exports = {
 
     testFunction: function () {
         console.log(`beep boop the zendesk testFunction fired`)
     },
-    getOrganizationData: async function getOrganizationData(requesterOrganizationID) {
+    getOrganizationData: async function getOrganizationData(requesterOrganizationID) { 
         console.log(`going to get organization data for ${requesterOrganizationID}`)
         //   use zendesk tool to get list of users 
 
-        console.log(`process.env ${process.env}`)
-
-        axios({
-            Authorization: 'Basic ' + Base64.encode(`${zendeskUsername}/token:${zendeskToken}`),
-            method: 'get',
-            url: zendeskURL,
-
-        }).then((data) => {
-            console.log(`data from zendesk api ${data}`)
-        }).catch((err) => {
-            console.log(err)
-        });
-
-
-
+        // console.log(`process.env ${JSON.stringify(process.env)}`)
 
 
 
