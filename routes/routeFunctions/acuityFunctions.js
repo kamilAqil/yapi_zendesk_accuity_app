04cdd51b40e756fc0caaad8d2446238d03f0a722
YAPI <yapi@Kamil-imac.localdomain>
2019-03-25 17:10:07 -0700

    end of workday push

--- routes/routeFunctions/acuityFunctions.js
+++ routes/routeFunctions/acuityFunctions.js
@@ -10,7 +10,7 @@ let acuity = Acuity.basic({
 var moment = require('moment');
 moment().format();
 
-const arrayOfAppointmentsForAllUsers = []
+const arrayOfAppointmentsForAllUsers = ['1']
 
 module.exports = {
   testFunction: function () {
@@ -139,101 +139,6 @@ module.exports = {
     }
 
 
-    // first get and set the acuity colors and appointment
-    // types 
-    await getAcuityColors().then((data) => {
-
-      // console.log(`got acuity colors ${JSON.stringify(data,null," ")}`)
-      data.forEach((el) => {
-        objOfColors[`${el['name']}`] = el['color'];
-
-      });
-    }).catch((err) => {
-      console.log(`Error getting colors ${err}`)
-    });
-
-    await getAcuityData2(arrayOfUsers, arrayOfAppointmentsForAllUsers).then((data) => {
-
-      console.log(`Got data from getAcuityData2() ${JSON.stringify(data, null, " ")}`)
-
-
-      let today = moment();
-      today.utc();
-
-
-      data.forEach((element) => {
-
-        // get the acuity color associated with appointment 
-        let colorOfAppt = objOfColors[`${element['type']}`]
-        let dateToTest = new Date(element['datetime']);
-
-        if (colorOfAppt === undefined) {
-          colorOfAppt = "black"
-        }
-
-        // trim acuity data here and return trimmed data
-        // for each object extract the appointment ID, name, 
-        // appointment date
-
-        let appointmentObjectToPush = {
-          id: element['id'],
-          name: element['firstName'],
-          date: element['date'],
-          dateTime: element['datetime'],
-          dateToTest: dateToTest,
-          assignedTo: element['calendar'],
-          time: element['time'],
-          endTime: element['endTime'],
-          type: element['type'],
-          color: colorOfAppt,
-          notes: element['notes'],
-          difference: undefined,
-          link: `https://secure.acuityscheduling.com/appointments/view/${element['id']}?backto=l:0`
-        }
-
-        // Get todays date to be able to organize 
-        // appointments by testing difference between appt
-        // date and today
-
-        let timeToCompare = moment(appointmentObjectToPush['dateToTest']);
-
-        timeToCompare.format();
-
-        let difference = timeToCompare.diff(today, 'hours');
-        appointmentObjectToPush['difference'] = difference
-
-        // test appointmentObject difference and if 
-        // difference is positive it is an upcoming appt
-        // if it is a negative difference then it is a past 
-        // appt and if there is no difference it is an appointment 
-        // today
-
-        if (appointmentObjectToPush['difference'] < 0) {
-          dataForFrontEnd.pastAppointments.push(appointmentObjectToPush)
-          console.log(`pushing appointment to past appointments difference is ${appointmentObjectToPush['difference']} and the date is ${appointmentObjectToPush['date']}\n`)
-        } else if (appointmentObjectToPush['difference'] == 0) {
-          if (moment(appointmentObjectToPush['dateTime']).isSame(today, 'day') == true) {
-            dataForFrontEnd.todaysAppointments.push(appointmentObjectToPush)
-            console.log(`SAME DAY !pushing appointment to today appointments difference is ${appointmentObjectToPush['difference']} and the date is ${appointmentObjectToPush['date']}\n`)
-          }
-          // dataForFrontEnd.todaysAppointments.push(appointmentObjectToPush)
-          // console.log(`pushing appointment to today appointments difference is ${appointmentObjectToPush['difference']} and the date is ${appointmentObjectToPush['date']}\n`)
-        } else if (appointmentObjectToPush['difference'] > 0) {
-
-          if (moment(appointmentObjectToPush['dateTime']).isSame(today, 'day') == false) {
-            dataForFrontEnd.futureAppointments.push(appointmentObjectToPush)
-            console.log(`pushing appointment to future appointments not the same day difference is ${appointmentObjectToPush['difference']} and the date is ${appointmentObjectToPush['date']}\n`)
-            console.log(`future appointment not on the same day ${appointmentObjectToPush['date']}\n`)
-          }
-          // dataForFrontEnd.futureAppointments.push(appointmentObjectToPush)
-          // console.log(`pushing appointment to future appointments not the same day difference is ${appointmentObjectToPush['difference']} and the date is ${appointmentObjectToPush['date']}`)
-        }
-        return dataForFrontEnd
-      });
-    }).catch((err) => {
-
-      console.log(`Acuity Promise err ${err}`);
-    });
 
     console.log(`Going to return dataForFrontEnd ${dataForFrontEnd}`)
     dataForFrontEnd.futureAppointments = dataForFrontEnd.futureAppointments.reverse();
@@ -272,7 +177,8 @@ function getAcuityData2(arrayOfUsers, arrayOfAppointmentsForAllUsers){
   // gets an array of users from an organization and calls acuity
   // to get the list of appointments for each user
   return new Promise((resolve,reject)=>{
-    resolve(`getAcuityData2 has been called with and this is the response ${arrayOfUsers}, ${arrayOfAppointmentsForAllUsers}`)
+    console.log(`arrayOfAppointmentsForAllUsers inside getAcuityData2 promise ${arrayOfAppointmentsForAllUsers}`)
+    resolve(`getAcuityData2 has been called with ${arrayOfUsers} and this is the response, ${arrayOfAppointmentsForAllUsers}`)
   })
   
 }
