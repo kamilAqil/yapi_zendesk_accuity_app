37b2de1bbe91fd2f9a2d6a52cd17d66812f0c50e
YAPI <yapi@Kamil-imac.localdomain>
2019-04-01 17:35:59 -0700

    going to search for organization on the server

---
+++ routes/acuityAPI3.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let router = express.Router();
+let Acuity = require('acuityscheduling');
+let userId = process.env.ACUITY_USER_ID;
+let apiKey = process.env.ACUITY_API_TOKEN;
+let acuityFunctions = require('./routeFunctions/acuityFunctions');
+let zendeskFunctions = require('./routeFunctions/zendeskFunctions');
+var Zendesk = require('zendesk-node-api');
+
+
+
+// var zendesk = new Zendesk({
+//     url: process.env.YOUR_ZENDESK_URL, // https://example.zendesk.com
+//     email: process.env.YOUR_ZENDESK_EMAIL, // me@example.com
+//     token: process.env.YOUR_ZENDESK_API_TOKEN // hfkUny3vgHCcV3UfuqMFZWDrLKms4z3W2f6ftjPT
+//   });
+
+var moment = require('moment');
+moment().format();
+
+
+
+
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+   
+
+});
+
+
+
+module.exports = router;
