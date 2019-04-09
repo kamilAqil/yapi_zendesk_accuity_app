e5dd439afb73ceb4c3a9ff1c9711fcfb8cea30dd
YAPI <yapi@Kamil-imac.localdomain>
2019-04-08 17:46:53 -0700

    log of extended middleware

--- app.js
+++ app.js
@@ -57,58 +57,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
 
-  // Only extend the timeout for API requests
-  if (!req.url.includes('/acuityAPI3')) {
-    next();
-    return;
-  }
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 450);
-  };
-
-  waitAndSend();
-  next();
-};
-
-app.use(extendTimeoutMiddleware);
 
 module.exports = app;
