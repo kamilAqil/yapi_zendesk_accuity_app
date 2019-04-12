fff0b053f6c4236e55bf12be9e0a332de99463fd
YAPI <yapi@Kamil-imac.localdomain>
2019-04-12 15:27:19 -0700

    using next js build on package json

--- routes/acuityAPI3.js
+++ routes/acuityAPI3.js
@@ -23,63 +23,7 @@ today.utc();
 const space = ' ';
 let isFinished = false;
 let isDataSent = false;
-/*
-const extendTimeoutMiddleware = (req, res, next) => {
-    console.log('EXTENDED TIMEOUT MIDDLEWARE')
-  
-   
-  
-    // Only extend the timeout for API requests
-    
-    // if (!req.url.includes('/')) {
-    //     console.log('wrong route')
-    //     next();
-    //     return;
-    // }
-  
-    res.once('finish', () => {
-        isFinished = true;
-    });
-  
-    res.once('end', () => {
-        isFinished = true;
-    });
-  
-    res.once('close', () => {
-        isFinished = true;
-    });
-  
-    res.on('data', (data) => {
-        // Look for something other than our blank space to indicate that real
-        // data is now being sent back to the client.
-        if (data !== space) {
-            isDataSent = true;
-        }
-    });
-  
-    const waitAndSend = () => {
-        setTimeout(() => {
-            // If the response hasn't finished and hasn't sent any data back....
-            if (!isFinished && !isDataSent) {
-                // Need to write the status code/headers if they haven't been sent yet.
-                if (!res.headersSent) {
-                    // res.writeHead(202);
-                }
-  
-                res.write(space);
-                console.log(`time extended`)
-                // Wait another 15 seconds
-                waitAndSend();
-            }
-        }, 200);
-    };
-  
-    waitAndSend();
-    next()
-  };
-  
-  router.use(extendTimeoutMiddleware);
-*/
+
 
 /* GET home page. */
 
